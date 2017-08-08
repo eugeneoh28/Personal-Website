0469728410d572ae5a0c125c80034ae10065e6ff
Eugene Oh <eo28@uw.edu>
2017-08-08 16:51:57 -0700

    initial thoughts added

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+  var navBurgers = Array.prototype.slice.call($('.navbar-burger'), 0);
+
+  if(navBurgers.length > 0) {
+    $('.navbar-burger').click(function(){
+      $(this).toggleClass('is-active');
+      $($(this).data('target')).toggleClass('is-active');
+    });
+  }
+  $('#hero-text').animate({opacity:1});
+});
