a9d0d48e2c24a57be198443002358346bb50ca90
Eugene <eugene.oh@realself.com>
2018-08-03 23:20:43 -0700

    new version beta

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
 var express = require("express"),
     app = express(),
-    bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    admin = require("firebase-admin"),
+    serviceAccount = require("./key.json"),
+    v1 = require("./v1.js");
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://personal-website-dc900.firebaseio.com"
+});
+var database = admin.database(),
+		ref = database.ref(),
+		data = {};
+
+ref.on("value", function(snapshot) {
+   data = snapshot.val();
+}, function (error) {
+   console.log("Error: " + error.code);
+});
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + "/public"))
 app.set("view engine", "ejs");
+app.use('/', v1);
 
 app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/work", function(req, res) {
-  res.render("work");
-});
-
-app.get("/work/:project", function(req, res){
-  res.render('work/'+req.params.project);
-});
-
 app.get("/about", function(req, res) {
   res.render("about");
+});
+
+app.get("/work", function(req, res) {
+
+  res.render("work", {data:data});
+});
+
+app.get("/work/tech/:project", function(req, res){
+  res.render('tech', {data:data['technical'][req.params.project]});
+});
+
+app.get("/work/service/:project", function(req, res){
+  res.render('service', {data:data['service'][req.params.project]});
 });
 
 app.get("/*", function(req, res) {
--- public/js/work.js
+++ public/js/work.js
@@ -1,29 +1,13 @@
 $(document).ready(function(){
-  var loadTab = sessionStorage.getItem("work");
-  if (loadTab == null || loadTab == "#technical") {
-    $('#titletechnical').addClass('activelink');
-    $('#technical').fadeIn(800).siblings().hide().fadeOut(800)
-  } else{
-    $('#titleservice').addClass('activelink');
-    $('#service').fadeIn(800).siblings().hide().fadeOut(800)
-  }
+  $('#titletechnical').addClass('active');
+  $('#technical').fadeIn(600).siblings().hide().fadeOut(600)
 });
 
-$('.tab-links a').on('click', function(e){
+$('.tabs li').on('click', function(e){
   var currAttr = $(this).attr('href');
+  console.log(currAttr);
+  $(currAttr).fadeIn(600).siblings().hide().fadeOut(600);
 
-  $('.tab'+currAttr).fadeIn(800).siblings().hide().fadeOut(800)
-
-  $(this).parent('li').addClass('activelink').siblings().removeClass('activelink');
-  sessionStorage.setItem("work", currAttr);
+  $(this).addClass('active').siblings().removeClass('active');
   e.preventDefault();
 });
-
-$('project-item').hover(
-  function(){
-      $('.project-text').fadeIn();
-  },
-  function(){
-      $('project-text').fadeOut();
-  }
-);
