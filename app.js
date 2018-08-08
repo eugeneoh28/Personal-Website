3b2de37daa64c1974d587857b04dac52b711c377
Eugene <eugene.oh@realself.com>
2018-08-08 00:40:03 -0700

    plz work

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var express = require("express"),
 
 admin.initializeApp({
   credential: admin.credential.cert({
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
     "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL
   }),
   databaseURL: "https://personal-website-dc900.firebaseio.com"
 });
