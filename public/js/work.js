1b575acccf8f7379952c1dfb0ecf8866bd61eaf1
Eugene <eugene.oh@realself.com>
2018-08-11 23:44:33 -0700

    janky will fix later but finished except resume

--- public/js/work.js
+++ public/js/work.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
 
 $('.tabs li').on('click', function(e){
   var currAttr = $(this).attr('href');
-  console.log(currAttr);
   $(currAttr).fadeIn(600).siblings().hide().fadeOut(600);
 
   $(this).addClass('active').siblings().removeClass('active');
